1f75568dc7a1c33b4990ab5cc8d5932028ee2551
GVLocke <jkeys726@gmail.com>
2024-02-08 09:15:39 -0600

    started work on class-7 separated class-6 js into separate file

---
+++ Week-5/Class-7/js/script.js
@@ -0,0 +1,8 @@
+function mylogin() {
+    document.getElementById("login-section").style.display="block";
+    document.getElementById("register-section").style.display="none";
+}
+function myregister() {
+    document.getElementById("register-section").style.display="block";
+    document.getElementById("login-section").style.display="none";
+}
